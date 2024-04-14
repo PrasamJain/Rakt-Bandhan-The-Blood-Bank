66570754b8466c2dba2b1e1189df6344a6d188ac
prasamjain <prasamjain294@gmail.com>
2024-04-14 10:10:12 +0530

    update

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -1,42 +1,96 @@
-import React from 'react'
-import Layout from '../components/shared/Layout/Layout'
-import "../index.css"
+import React from 'react';
+import Layout from '../components/shared/Layout/Layout';
+import '../index.css';
 
 const AboutPage = () => {
     return (
         <Layout>
             <div className="container">
-                <h1 style={{ fontFamily: "fantasy" }}>
-                    About Page
-                </h1 >
-                {/* <div className='d-felx flex-column mt-4'>
-                    <h1 style={{ fontFamily: "fantasy" }}>
-                        About Page
-                    </h1 >
-                    <h3>Made with <i class="fa-solid fa-heart"></i> By Rahul Bairagi </h3>
-                    <hr />
-                    <p style={{ justifyContent: "flex-start" }}>
-                        The blood bank website project developed by Rahul Bairagi is built on the MERN stack, which stands for MongoDB, Express.js, React, and Node.js. This technology stack is widely used for building robust and scalable web applications.
-                        <hr />
-                        A blood bank website is an online platform that serves as a centralized resource for connecting blood donors with recipients in need. It is designed to facilitate the process of blood donation, streamline communication, and ensure efficient management of the blood supply. The website acts as a bridge between donors and healthcare institutions, providing valuable information, online registration, scheduling, and other essential features.
-                    </p>
-                    <h3>Contacts</h3>
+                {/* Blood Bank Information */}
+                <div className="blood-bank-info" style={{ textAlign: 'center', marginTop: '2rem' }}>
+                    {/* <img src="/blood_bank_logo.png" alt="Blood Bank Logo" style={{ width: '200px', marginBottom: '1rem' }} /> */}
+                    <h2>About the Rakt Bandhan : The Blood Bank</h2>
                     <p>
-                        <i class="fa-solid fa-brands fa-google text-success "></i>
-                        <a href='https://mail.google.com/mail/u/0/#inbox?compose=new' className='anc' target="_blank">Gmail : rahulbairagiofficial@gmail.com</a>
+                        The Blood Bank is a leading organization dedicated to ensuring a safe and reliable blood supply for medical emergencies and healthcare institutions. Our mission is to save lives by connecting generous donors with those in need of blood transfusions.
                     </p>
-                    <p>
-                        <i class="fa-solid fa-brands fa-github text-success"></i>
-                        <a href='https://github.com/rahulcode751' className='anc' target="_blank">GitHub : rahulcode751</a>
-                    </p>
-                    <p>
-                        <i class="fa-solid fa-brands fa-linkedin text-success "></i>
-                        <a href='https://www.linkedin.com/in/rahul3008/' className='anc' target="_blank">LinkedIn : Rahul Bairagi</a>
-                    </p>
-                </div > */}
-            </div >
-        </Layout >
-    )
+                </div>
+
+                {/* Our Team Section */}
+                <div className="our-team-section" style={{ marginTop: '3rem' }}>
+                    <h2 style={{ textAlign: 'center' }}>Our Team</h2>
+                    <div className="row justify-content-center">
+                        {/* Prasam Jain Card */}
+                        <div className="col-md-4 mb-3">
+                            <div className="card">
+                                <img
+                                    src="./assets/images/prasam.jpg"
+                                    className="card-img-top rounded-circle"
+                                    alt="Prasam Jain"
+                                    style={{
+                                        width: '130px',
+                                        height: '130px',
+                                        margin: 'auto', // Center horizontally
+                                        display: 'block',
+                                        marginTop: '20px' // Ensures proper alignment
+                                    }}
+                                />
+
+                                <div className="card-body" >
+                                    <h5 style={{ textAlign: 'center' }}>Prasam Jain (Founder & CEO)</h5>
+                                    <p className="card-text" style={{ textAlign: 'center' }}>Full Stack Developer</p>
+                                    <ul className="list-unstyled">
+                                        <li><em>I am a quick learner and problem solver & I believe that
+                                            I can contribute to your team by bringing fresh perspectives,
+                                            and a willingness to learn and grow.</em></li>
+                                    </ul>
+                                </div>
+                                <div className="card-footer text-center">
+                                    <a href="mailto:prasamjain294@gamil.com"><i className="fa-solid fa-envelope text-success me-3"></i></a>
+                                    <a href="https://github.com/prasamjain"><i className="fa-brands fa-github text-success me-3"></i></a>
+                                    <a href="https://www.linkedin.com/in/prasam-jain-59a45a191"><i className="fa-brands fa-linkedin text-success me-3"></i></a>
+                                    <span>Phone: 6265973186</span>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* Palash Mishra Card */}
+                        <div className="col-md-4 mb-3">
+                            <div className="card">
+                                <img
+                                    src="./assets/images/palash.jpeg"
+                                    className="card-img-top rounded-circle"
+                                    alt="Prasam Jain"
+                                    style={{
+                                        width: '130px',
+                                        height: '130px',
+                                        margin: 'auto', // Center horizontally
+                                        display: 'block',
+                                        marginTop: '20px' // Ensures proper alignment
+                                    }}
+                                />
+                                <div className="card-body">
+                                    <h5 style={{ textAlign: 'center' }}>Palash Mishra (Co-Founder)</h5>
+                                    <p className="card-text" style={{ textAlign: 'center' }}>Fronted Designer</p>
+                                    <ul className="list-unstyled">
+                                        <li><em>I am excited about the opportunity to work as an intern 
+                                            at your company and I am confident that I can make valuable 
+                                            contributions to your projects.</em></li>
+                                    </ul>
+                                </div>
+                                <div className="card-footer text-center">
+                                    <a href="mailto:palashmishra54@gmail.com"><i className="fa-solid fa-envelope text-success me-3"></i></a>
+                                    <a href="https://github.com/palash0216"><i className="fa-brands fa-github text-success me-3"></i></a>
+                                    <a href="https://www.linkedin.com/in/palash-mishra-2a5877204/"><i className="fa-brands fa-linkedin text-success me-3"></i></a>
+                                    <span>Phone: 7049991771</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </Layout>
+    );
 }
 
-export default AboutPage
+export default AboutPage;
--- client/src/pages/Admin/AdminHome.js
+++ client/src/pages/Admin/AdminHome.js
@@ -1,29 +1,39 @@
-import React from 'react'
-import Layout from '../../components/shared/Layout/Layout'
+import React from 'react';
+import Layout from '../../components/shared/Layout/Layout';
 import { useSelector } from 'react-redux';
 
 const AdminHome = () => {
     const { user } = useSelector((state) => state.auth);
+
     return (
         <Layout>
             <div className='container'>
-                <div className='d-felx flex-column mt-4'>
-                    <h1>
-                        Welcome Admin <i className="text-success">{user?.name}</i>
+                <div className='d-flex flex-column mt-4'>
+                    <h1 style={{ color: '#2c3e50', marginBottom: '20px' }}>
+                        Welcome Admin <span style={{ color: '#27ae60' }}>{user?.name}</span>
                     </h1>
-                    <h3>Manage Blood Bank App </h3>
-                    <hr />
-                    <p>
-                        The blood bank  website project developed by palash , prasam and Rahul Bairagi is built on the MERN stack, which stands for MongoDB, Express.js, React, and Node.js. This technology stack is widely used for building robust and scalable web applications.
-                        <hr />
-                        A blood bank website is an online platform that serves as a centralized resource for connecting blood donors with recipients in need. It is designed to facilitate the process of blood donation, streamline communication, and ensure efficient management of the blood supply. The website acts as a bridge between donors and healthcare institutions, providing valuable information, online registration, scheduling, and other essential features.
-                        <hr />
+                    <h3 style={{ color: '#2980b9', marginBottom: '10px' }}>Manage Blood Bank App</h3>
+                    <hr style={{ marginBottom: '20px' }} />
+                    <p style={{ color: '#34495e' }}>
+                        The blood bank website project developed by Palash, Prasam, and Rahul Bairagi is built on the MERN stack,
+                        which stands for MongoDB, Express.js, React, and Node.js. This technology stack is widely used for building
+                        robust and scalable web applications.
                     </p>
-
+                    <hr style={{ marginBottom: '20px' }} />
+                    <p style={{ color: '#34495e' }}>
+                        A blood bank website is an online platform that serves as a centralized resource for connecting blood donors
+                        with recipients in need. It is designed to facilitate the process of blood donation, streamline communication,
+                        and ensure efficient management of the blood supply. The website acts as a bridge between donors and healthcare
+                        institutions, providing valuable information, online registration, scheduling, and other essential features.
+                    </p>
+                    <hr style={{ marginBottom: '20px' }} />
+                    <p style={{ color: '#34495e', fontStyle: 'italic' }}>
+                        "The gift of blood is the gift of life."
+                    </p>
                 </div>
             </div>
         </Layout>
-    )
-}
+    );
+};
 
-export default AdminHome
+export default AdminHome;
